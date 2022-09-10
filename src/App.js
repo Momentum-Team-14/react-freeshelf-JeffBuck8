b112b2b454a695301f9f5caa7c630233edbd3ef6
JeffBuck8 <jdbuckcode@gmail.com>
2022-09-10 14:31:19 -0400

    Worked out issues with book images, added a font, and styled the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import { books } from "./books"
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export const App = () => {
+    return(
+        <>
+            <h1>Program Language Books!</h1>
+            <div className="book-list">
+                {books.map((book, index) => (
+                    <Book title={book.title} author={book.author} url={book.url} shortDescription={book.shortDescription} coverImageUrl={book.coverImageUrl} publisher={book.publisher} publicationDate={book.publicationDate} detailedDescription={book.detailedDescription} 
+                    />
+                ))}
+            </div>
+        </>
+    )
 }
 
-export default App;
+const Book = ({ title, author, url, shortDescription, coverImageUrl, publisher, publicationDate, detailedDescription }) => {
+    const [expanded, setExpanded] = useState(false)
+
+    const handleClick = () => {
+        setExpanded(!expanded)
+    }
+
+    return(
+        <div style={{ border: '2px solid grey', margin: '10px', padding: '5px',}}>
+            <div className="mainInfo">
+                <div>
+                    { title ? <p className='title'> {title} </p> : "" }
+                    { author ? <p> {author} </p> : "" }
+                    { shortDescription ? <p> {shortDescription} </p> : "" }
+                </div>
+                <div>
+                    { coverImageUrl ? <p className='cover'> <img src={coverImageUrl} alt=""></img> </p> : "" }
+                </div>
+            </div>
+            <button onClick={() => handleClick()} aria-expanded={ expanded }>
+                {expanded ? 'Less' : 'More'} info
+            </button>
+            {expanded && url ? <p>URL: <a href={url}>{url}</a></p> : ''}
+            {expanded && publisher ? <p>Publisher: {publisher}</p> : ''}
+            {expanded && publicationDate ? <p>Publication Date: {publicationDate}</p> : ''}
+            {expanded && detailedDescription ? <p>Full Description: {detailedDescription}</p> : ''}
+        </div>
+    )
+}
